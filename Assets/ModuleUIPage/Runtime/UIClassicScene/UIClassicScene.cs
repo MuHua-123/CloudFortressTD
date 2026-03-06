4bd446514ba81a28081eb6758129811267f2fb5c
MuHua-123 <136542559+MuHua-123@users.noreply.github.com>
2026-03-06 14:00:53 +0800

    1

--- Assets/ModuleUIPage/Runtime/UIClassicScene/UIClassicScene.cs
+++ Assets/ModuleUIPage/Runtime/UIClassicScene/UIClassicScene.cs
@@ -7,8 +7,13 @@ using MuHua;
 /// <summary>
 /// 经典模式 - 选择场景
 /// </summary>
-public class UIClassicScene : ModuleUIPage {
+public class UIClassicScene : UIStandardPage {
 
 	public override VisualElement Element => root.Q<VisualElement>("ClassicScene");
 
+	protected override void ModuleUI_OnJumpPage(Page page) {
+		bool isEnable = page == Page.ClassicScene;
+		Element.EnableInClassList("document-page-hide", !isEnable);
+		if (!isEnable) { return; }
+	}
 }
--- Assets/ModuleUIPage/Runtime/UIHomePage/UIHomePage.cs
+++ Assets/ModuleUIPage/Runtime/UIHomePage/UIHomePage.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,24 @@ using MuHua;
 /// <summary>
 /// 主页
 /// </summary>
-public class UIHomePage : ModuleUIPage {
+public class UIHomePage : UIStandardPage {
 
 	public override VisualElement Element => root.Q<VisualElement>("HomePage");
+
+	public Button Button1 => Q<Button>("Button1");// 经典模式
+
+	public override void Awake() {
+		base.Awake();
+		Button1.clicked += Button1_clicked;
+	}
+
+	protected override void ModuleUI_OnJumpPage(Page page) {
+		bool isEnable = page == Page.Home;
+		Element.EnableInClassList("document-page-hide", !isEnable);
+		if (!isEnable) { return; }
+	}
+
+	private void Button1_clicked() {
+
+	}
 }
